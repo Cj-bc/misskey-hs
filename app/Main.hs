4d7751ce4e9b11537f3dbc045d7d3ef6b03b5657
Cj-bc <cj.bc-sd@outlook.jp>
2019-12-27 01:49:54 +0900

    Add: app/Main> simple program to call user/show API with my account info; add 'unicode-show' for dep

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
+import Data.Aeson (encode, object, (.=), Value)
+import Text.Show.Unicode (ushow)
+import Network.HTTP.Client
+import Network.HTTP.Simple
+import Network.Misskey.Type (User)
 
-import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+    let requestObject = object ["username" .= ("cj_bc_sd" :: String)]
+    initReq <- parseRequest "https://virtual-kaf.fun/api/users/show"
+    let request = initReq { method = "POST"
+                          , requestBody = RequestBodyLBS $ encode requestObject
+                          , requestHeaders =
+                                [("Content-Type", "application/json; charset=utf-8")]
+                          }
+
+    response <- httpJSON request
+    putStrLn $ ushow (getResponseBody response :: User)
