c072d5d2051c34ea3f3f5ba780594901c134b7a4
Cj-bc <cj.bc-sd@outlook.jp>
2019-12-27 23:35:18 +0900

    Change: app/Main> use implemented library to call API [WIP]

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
-import Data.Aeson (encode, object, (.=), Value)
+import Control.Monad.Trans.Reader (runReaderT)
 import Text.Show.Unicode (ushow)
 import Network.HTTP.Client
 import Network.HTTP.Simple
-import Network.Misskey.Type (User)
+import Network.Misskey.Type
+import Network.Misskey.Api.Users.Show
 
 
 main :: IO ()
 main = do
-    let requestObject = object ["username" .= ("cj_bc_sd" :: String)]
-    initReq <- parseRequest "https://virtual-kaf.fun/api/users/show"
-    let request = initReq { method = "POST"
-                          , requestBody = RequestBodyLBS $ encode requestObject
-                          , requestHeaders =
-                                [("Content-Type", "application/json; charset=utf-8")]
-                          }
+    let env = MisskeyEnv "" "https://virtual-kaf.fun"
+        request = UserName "cj_bc_sd"
 
-    response <- httpJSON request
-    putStrLn $ ushow (getResponseBody response :: User)
+    response <- runReaderT (usersShow request) env
+    case response of
+        Left er -> putStrLn $ "Error occured: " ++ show er
+        Right u -> putStrLn $ show u
