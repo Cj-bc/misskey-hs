1b16b81f2dac5b1ffd4e5f5076344bed9ac0c74b
Cj-bc <cj.bc-sd@outlook.jp>
2020-01-01 22:15:49 +0900

    Change: app/Main> will exec 'users/{show,notes}' to argument 'url' 'username'

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 import Text.Show.Unicode (ushow)
+import System.Environment (getArgs)
 import Network.HTTP.Client
 import Network.HTTP.Simple
 import Network.Misskey.Type
-import Network.Misskey.Api.Users.Show
+import qualified Network.Misskey.Api.Users.Notes as UN
+import qualified Network.Misskey.Api.Users.Show as USh
+import Lens.Simple ((^.))
 
 
 main :: IO ()
 main = do
-    let env = MisskeyEnv "" "https://virtual-kaf.fun"
-        request = UserName "cj_bc_sd"
-
-    response <- runMisskey (usersShow request) env
+    (domain:name:_) <- getArgs
+    let env = MisskeyEnv "" $ "https://" ++ domain
+    print "========== users/show =========="
+    response <- runMisskey (USh.usersShow (USh.UserName name Nothing)) env
     case response of
-        Left er -> putStrLn $ "Error occured: " ++ show er
-        Right u -> print u
+        Left er   -> print $ "Error occured while users/show: " ++ ushow er
+        Right usr -> do
+            let request = UN.APIRequest (head usr ^.user_id) Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing
+            notes <- runMisskey (UN.usersNotes request) env
+            case notes of
+                Left er -> print $ "Error occured while users/notes: " ++ ushow er
+                Right notes -> (putStrLn . ushow) notes
+
