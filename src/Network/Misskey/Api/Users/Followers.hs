9f728fcc1d88b7a7cde969b3d00401f3c0c4c461
Cj-bc <cj.bc-sd@outlook.jp>
2020-01-03 17:32:16 +0900

    Add: Network.Misskey.Api.Users.Followers> API endpoint for 'users/followers'

---
+++ src/Network/Misskey/Api/Users/Followers.hs
@@ -0,0 +1,43 @@
+{-# Language TemplateHaskell #-}
+{-# Language OverloadedStrings #-}
+{-|
+Module      : Network.Misskey.Api.Users.Followers
+Description : Misskey API Endpoint and Request for users/followers
+Copyright   : (c) Cj.bc_sd a.k.a Cj-bc, 2020
+Maintainer  : cj.bc-sd@outlook.jp
+Stability   : experimental
+
+Call `users/followers` Misskey API
+API document is: https://misskey.io/api-doc#operation/users/followers
+-}
+module Network.Misskey.Api.Users.Followers (
+  APIRequest(APIRequest)
+, usersFollowers
+) where
+
+import Data.Aeson (object)
+import Lens.Simple ((^.), makeLenses)
+import Network.Misskey.Type
+import Network.Misskey.Api.Internal
+
+
+data APIRequest = APIRequest { _userId   :: Maybe String
+                             , _username :: Maybe String
+                             , _host     :: Maybe String
+                             , _sinceId  :: Maybe String
+                             , _untilId  :: Maybe String
+                             , _limit    :: Maybe Int
+                             }
+makeLenses ''APIRequest
+
+-- | Call 'users/followers' API and return result
+usersFollowers :: APIRequest -> Misskey [User]
+usersFollowers req = postRequest "/api/users/Followers" obj
+        where
+            userIdObj   = createMaybeObj "userId"   (req^.userId)
+            usernameObj = createMaybeObj "username" (req^.username)
+            hostObj     = createMaybeObj "host"     (req^.host)
+            sinceIdObj  = createMaybeObj "sinceId"  (req^.sinceId)
+            untilIdObj  = createMaybeObj "untilId"  (req^.untilId)
+            limitObj    = createMaybeObj "limit"    (req^.limit)
+            obj = object $ mconcat [userIdObj, usernameObj, hostObj, sinceIdObj, untilIdObj, limitObj]
