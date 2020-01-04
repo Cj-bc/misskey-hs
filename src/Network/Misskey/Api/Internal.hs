49e9fb7ee13f781329ef504d43208307af997b2d
Cj-bc <cj.bc-sd@outlook.jp>
2020-01-04 11:14:00 +0900

    Change: Network.Misskey.Api.Internal> use httpJSON instead of httpLbs
    This was reverted once because something was going wrong.
    But now, I re-revert it because it works for now.

--- src/Network/Misskey/Api/Internal.hs
+++ src/Network/Misskey/Api/Internal.hs
@@ -1,12 +1,13 @@
 {-# Language OverloadedStrings #-}
 module Network.Misskey.Api.Internal where
 
-import Control.Monad.Trans.Reader (ask)
-import Data.Aeson (Value, encode, FromJSON, eitherDecode', (.=))
+import Control.Monad.IO.Class (liftIO, MonadIO)
+import Control.Monad.Trans.Reader (ask, ReaderT)
+import Data.Aeson (Value, encode, FromJSON, (.=), fromJSON, Result(..))
 import Lens.Simple ((^.))
 import Network.HTTP.Client (method, requestBody, RequestBody(RequestBodyLBS), requestHeaders
                            , Response, parseRequest)
-import Network.HTTP.Simple (httpLbs, httpJSON, getResponseBody, getResponseStatusCode)
+import Network.HTTP.Simple (httpJSON, getResponseBody, getResponseStatusCode)
 
 import Network.Misskey.Type
 
@@ -34,25 +35,26 @@ postRequest apiPath body =  do
                                       [("Content-Type", "application/json; charset=utf-8")]
                                 }
 
-    response <- httpLbs request
+    response <- httpJSON request :: MonadIO m => m (Response Value)
 
     let responseBody = getResponseBody response
     case getResponseStatusCode response of
-        200 -> case eitherDecode' responseBody of
-                Right a ->  return $ Right a
-                Left e -> error $ unlines [apiPath ++ ": error while decoding Result"
-                                          , "FATAL: Please file those outputs to author(or PR is welcome)."
-                                          , "========== raw ByteString =========="
-                                          , show responseBody
-                                          , "========== Error message =========="
-                                          , show e
-                                          ]
-        _   -> case (eitherDecode' responseBody :: Either String APIError) of
-                Right a -> return $ Left a
-                Left e  -> error $ unlines [apiPath ++ ": error while decoding APIError"
-                                           , "FATAL: Please file those outputs to author(or PR is welcome)."
-                                           , "========== raw ByteString =========="
-                                           , show responseBody
-                                           , "========== Error message =========="
-                                           , show e
-                                           ]
+        200 -> case fromJSON responseBody of
+                    Success a' -> return $ Right a'
+                    Error   e  -> error  $ unlines [apiPath ++ ": error while decoding Result"
+                                                   , "FATAL: Please file those outputs to author(or PR is welcome)."
+                                                   , "========== raw ByteString =========="
+                                                   , show responseBody
+                                                   , "========== Error message =========="
+                                                   , show e
+                                                   ]
+        _   -> case fromJSON responseBody of
+                    Success a' -> return $ Left a'
+                    Error   e  -> error  $ unlines [apiPath ++ ": error while decoding APIError"
+                                                   , "FATAL: Please file those outputs to author(or PR is welcome)."
+                                                   , "========== raw ByteString =========="
+                                                   , show responseBody
+                                                   , "========== Error message =========="
+                                                   , show e
+                                                   ]
+
